1199c8b026fe786837c7d8049b2d485385b25a65
Jan S <jan.schmitter@gmail.com>
2018-07-18 16:16:34 +0200

    Shader Cleanup

--- include/d3d11/D3D11Shaders.h
+++ include/d3d11/D3D11Shaders.h
@@ -24,49 +24,42 @@ const char d3d11_texture_shader[] =
 	"struct VS_Input"
 	"{"
 	"	float2 pos : POSITION;"
-	"	float2 texCoords : TEXCOORD;"
+	"	float2 texCoord : TEXCOORD;"
 	"};"
 	"struct PS_Input"
 	"{"
 	"	float4 pos : SV_POSITION;"
-	"	float2 texCoords : TEXCOORD;"
+	"	float2 texCoord : TEXCOORD;"
 	"};"
-	"PS_Input VS(VS_Input vertex)"
+	"PS_Input VS(VS_Input input)"
 	"{"
-	"	PS_Input vsOut = (PS_Input)0;"
-	"	vsOut.pos = float4(vertex.pos.x, vertex.pos.y, 0.0f, 1.0f);"
-	"	vsOut.texCoords = vertex.texCoords;"
-	"	return vsOut;"
+	"	PS_Input output;"
+	"	output.pos = float4(input.pos.x, input.pos.y, 0.0f, 1.0f);"
+	"	output.texCoord = input.texCoord;"
+	"	return output;"
 	"}"
-	"float4 PS(PS_Input frag) : SV_TARGET"
+	"float4 PS(PS_Input input) : SV_TARGET"
 	"{"
-	"	return colorMap.Sample(colorSampler, frag.texCoords);"
+	"	return colorMap.Sample(colorSampler, input.texCoord);"
 	"}";
 
 // Line    
 static char d3d11_line_shaders[] =
-    "struct VSOut"
+    "struct PS_Input"
     "{"
-    "	float4 Col : COLOR;"
-    "	float4 Pos : SV_POSITION;"
+    "	float4 col : COLOR;"
+    "	float4 pos : SV_POSITION;"
     "};"
 
-    "VSOut VS(float4 Col : COLOR, float4 Pos : POSITION)"
+    "PS_Input VS(float4 pos : POSITION, float4 col : COLOR)"
     "{"
-    "	VSOut Output;"
-    "	Output.Pos = Pos;"
-    "	Output.Col = Col;"
-    "	return Output;"
+    "	PS_Input output;"
+    "	output.pos = pos;"
+    "	output.col = col;"
+    "	return output;"
     "}"
 
-    "float4 PS(float4 Col : COLOR) : SV_TARGET"
+    "float4 PS(PS_Input input) : SV_TARGET"
     "{"
-    "	return Col;"
+    "	return input.col;"
     "}";
-
-// Texture Input Layout
-static D3D11_INPUT_ELEMENT_DESC d3d11_texture_input[] =
-{
-	{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-	{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 }
-};
